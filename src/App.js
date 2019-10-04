ba24cc88e26941c499ebf13934dd6826007f0599
Andrew <8879789+manleyac@users.noreply.github.com>
2019-10-04 15:01:43 -0400

    updating shop page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import React from 'react';
+//dependencies
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+
+//components
 import HomePage from "./pages/homepage/homepage.component";
-import './App.css';
+import ShopPage from "./pages/shop/shop.component";
+
+//styles
+import "./App.css";
 
 function App() {
   return (
-    <div >
-      <HomePage />
+    <div>
+      <Switch>
+        <Route exact path="/shop" component={ShopPage} />
+        <Route path="/" component={HomePage} />
+      </Switch>
     </div>
   );
 }
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -17,3 +17,4 @@ const MenuItem = ({ title, imageUrl, size }) => (
 );
 
 export default MenuItem;
+ 
